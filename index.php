1a4c20e5c8ecd9420485bfb581b9557fe5f1f936
Nugraha Adi Sulistyo <46425489+nugrahaas@users.noreply.github.com>
2022-12-22 14:13:59 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>gamernobudget</title>
+	<link rel="stylesheet" type="text/css" href="css/index.css">
+
+</head>
+<body>
+
+	<div class="luaran">
+		<div class="container">
+			<li><a href="#" class="cool-link" id="cool-link">Game</a></li>
+			<li><a href="#" class="cool-link" id="cool-link">What's New</a></li>
+			<li><a href="#" class="cool-link" id="cool-link">About</a></li>
+
+		</div>
+		
+		<div>
+			<input></input>
+		</div>
+
+		<div>
+			
+			<p style="color: #fff; font-family: gill sans mt; font-size: 25px; margin-left: 10%; position: absolute; margin-top: 200px;">Trending</p>
+			<img src="image/right-arrow-white.png" style="position: absolute; color: #fff; width: 30px; background-color: ; margin-top: 200px; left: 20%">
+
+		</div>
+
+		<div class="card-game" style="background: #fff; width: 220px; height: 300px; margin-top: 280px; margin-left: 10%; position: absolute;">
+			<img src="image/rdr2.jpg" style="width: inherit; height: inherit;">
+		</div>
+
+		<div class="card-game" style="background: #fff; width: 220px; height: 300px; margin-top: 280px; margin-left: 30%; position: absolute;">
+			<img src="image/gow.jpg" style="width: inherit; height: inherit;">
+		</div>
+
+		<div class="card-game" style="background: #fff; width: 220px; height: 300px; margin-top: 280px; margin-left: 50%; position: absolute;">
+			<img src="image/horizon.jpg" style="width: inherit; height: inherit;">
+		</div>
+
+		<div class="card-game" style="background: #fff; width: 220px; height: 300px; margin-top: 280px; margin-left: 70%; position: absolute;">
+			<img src="image/got.jpg" style="width: inherit; height: inherit;">
+		</div>
+
+	</div>
+</body>
+</html>
