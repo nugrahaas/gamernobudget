b40d9c037a54be23e03e32db6224696bafc6f759
nurdilafarha <nurdilafarha@gmail.com>
2022-12-25 11:29:56 +0700

    update file

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
 	<title>gamernobudget</title>
 	<link rel="stylesheet" type="text/css" href="css/index.css">
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 
 </head>
 <body>
@@ -13,16 +14,35 @@
 			<li><a href="#" class="cool-link" id="cool-link">What's New</a></li>
 			<li><a href="#" class="cool-link" id="cool-link">About</a></li>
 
+				<form action="" class="search-box">
+					<input type="text" name="search" placeholder="Search Game here.." class="search-input"/>
+					<button class="w3-btn w3-round-xxlarge">
+						Search
+					</button>
+				</form>
+			
 		</div>
 		
 		<div>
-			<input></input>
+			<nav class="navigation">
+				<input type="checkbox" class="menu-btn" id="menu-btn">
+				<label for="menu-btn" class="menu-icon">
+					<span class="nav-icon"></span>
+				</label>
+
+				<a class="logo">
+					Gamernobudget.com
+				</a>
+
+			</nav>
 		</div>
+
+		
 
 		<div>
 			
 			<p style="color: #fff; font-family: gill sans mt; font-size: 25px; margin-left: 10%; position: absolute; margin-top: 200px;">Trending</p>
-			<img src="image/right-arrow-white.png" style="position: absolute; color: #fff; width: 30px; background-color: ; margin-top: 200px; left: 20%">
+			<img src="image/right-arrow-white.png" style="position: absolute; color: #fff; width: 30px; margin-top: 200px; left: 20%">
 
 		</div>
 
@@ -40,6 +60,30 @@
 
 		<div class="card-game" style="background: #fff; width: 220px; height: 300px; margin-top: 280px; margin-left: 70%; position: absolute;">
 			<img src="image/got.jpg" style="width: inherit; height: inherit;">
+		</div>
+
+
+		<div>
+			
+			<p style="color: #fff; font-family: gill sans mt; font-size: 25px; margin-left: 10%; position: absolute; margin-top: 610px;">For You</p>
+			<img src="image/right-arrow-white.png" style="position: absolute; color: #fff; width: 30px; margin-top: 610px; left: 20%">
+
+		</div>
+
+		<div class="card-game" style="background: #fff; width: 220px; height: 300px; margin-top: 700px; margin-left: 10%; position: absolute;">
+			<img src="image/gow.jpg" style="width: inherit; height: inherit;">
+		</div>
+
+		<div class="card-game" style="background: #fff; width: 220px; height: 300px; margin-top: 700px; margin-left: 30%; position: absolute;">
+			<img src="image/rdr2.jpg" style="width: inherit; height: inherit;">
+		</div>
+
+		<div class="card-game" style="background: #fff; width: 220px; height: 300px; margin-top: 700px; margin-left: 50%; position: absolute;">
+			<img src="image/got.jpg" style="width: inherit; height: inherit;">
+		</div>
+
+		<div class="card-game" style="background: #fff; width: 220px; height: 300px; margin-top: 700px; margin-left: 70%; position: absolute;">
+			<img src="image/horizon.jpg" style="width: inherit; height: inherit;">
 		</div>
 
 	</div>
